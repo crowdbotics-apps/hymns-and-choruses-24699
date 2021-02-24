a35be198773f5e46c83e2c74daa6bb2a04767a5f
Crowdbotics <team@crowdbotics.com>
2021-02-24 17:15:49 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/hymns_and_choruses_24699/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hymns_and_choruses_24699;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "hymns_and_choruses_24699";
+    }
+}
---
+++ ios/hymns_and_choruses_24699Tests/hymns_and_choruses_24699Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface hymns_and_choruses_24699 : XCTestCase
+
+@end
+
+@implementation hymns_and_choruses_24699
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
